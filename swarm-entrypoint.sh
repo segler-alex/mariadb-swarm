577544c46e553b28f354ed500ae263be02e4b3cd
Alexander Murauer <alexander.murauer@imagination.at>
2017-02-09 15:56:15 +0100

    join only other ips

--- swarm-entrypoint.sh
+++ swarm-entrypoint.sh
@@ -15,19 +15,40 @@ echo "Startup..1"
 
 HOSTS=$(dig +short $DNS)
 HOST_NUMBER=$(echo "$HOSTS" | wc -l)
+OWN_IPS=$(hostname -I)
 
 echo "Startup..2"
 
 if [ $HOST_NUMBER -gt 0 ]; then
     echo "Startup..3"
+    HOSTS_LINE_SPACE=$(echo "$HOSTS" | xargs)
     HOSTS_LINE=$(echo "$HOSTS" | xargs | sed -e 's/ /,/g')
 
+    for HOSTS_LINE_SPACE_I in $HOSTS_LINE_SPACE; do
+        FOUND=0
+        for OWN_IPS_I in $OWN_IPS; do
+            if [[ "$HOSTS_LINE_SPACE_I" == "$OWN_IPS_I" ]]; then
+                FOUND=1
+            fi
+        done
+
+        if [ $FOUND -eq 0 ]; then
+            if [ -z $GCOMM ]; then
+                GCOMM="${HOSTS_LINE_SPACE_I}"
+            else
+                GCOMM="${GCOMM},${HOSTS_LINE_SPACE_I}"
+            fi
+        fi
+    done
+
     echo "Line: $HOSTS_LINE"
+    echo "Own Ips: $OWN_IPS"
     echo "Found hosts: $HOST_NUMBER"
+    echo "Found other hosts: $GCOMM"
 
     if [ $HOST_NUMBER -gt 1 ]; then
-        echo "*** JOIN CLUSTER @ $HOSTS_LINE"
-        docker-entrypoint.sh "$@" "--wsrep-cluster-name=$CLUSTER_NAME" "--wsrep-cluster-address=gcomm://$HOSTS_LINE" "--wsrep-sst-auth=${WSREP_SST_AUTH}"
+        echo "*** JOIN CLUSTER @ $GCOMM"
+        docker-entrypoint.sh "$@" "--wsrep-cluster-name=$CLUSTER_NAME" "--wsrep-cluster-address=gcomm://$GCOMM" "--wsrep-sst-auth=${WSREP_SST_AUTH}"
     else
         echo "*** START NEW CLUSTER"
         docker-entrypoint.sh "$@" --wsrep-new-cluster "--wsrep-cluster-name=$CLUSTER_NAME" "--wsrep-cluster-address=gcomm://" "--wsrep-sst-auth=${WSREP_SST_AUTH}"
